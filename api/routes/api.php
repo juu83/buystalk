b1c5ce17192828531af862e4159a5ec230240d2f
root <root@laptop.localdomain>
2026-04-20 10:04:32 +0200

    fix : api

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,34 +1,41 @@
-<?php 
+<?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\AdController;
 
+Route::get('/ping', function () {
+  return response()->json([
+      'message' => 'API OK',
+      'time' => now()
+  ]);
+});
 
+
+Route::apiResource('posts', PostController::class)->only(['index', 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+  Route::apiResource('posts', PostController::class)->only(['store', 'update', 'destroy']);
+  Route::post('/user/ads', [AdController::class, 'store']);
+});
+
+Route::get('/ads', [AdController::class, 'index']);
 
 Route::post('/login', [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [UserController::class, 'logout']);
-
     Route::get('/user', [UserController::class, 'show']);
     Route::put('/user', [UserController::class, 'update']);
     Route::put('/user/password', [UserController::class, 'updatePassword']);
-    Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+    Route::post('/user/avatar', [UserController::class, 'updateAvatar']);  
+
     Route::get('/user/posts', [PostController::class, 'indexUser']);
-    Route::post('/user/posts', [PostController::class, 'store']);
     Route::get('/user/posts/{id}', [PostController::class, 'showUser']);
+    Route::post('/user/posts', [PostController::class, 'store']);
     Route::put('/user/posts/{id}', [PostController::class, 'update']);
     Route::delete('/user/posts/{id}', [PostController::class, 'destroy']);
-
-    Route::get('/user/ads', [AdController::class, 'indexUser']); 
-    Route::post('/user/ads', [AdController::class, 'store']);  
-    Route::get('/user/ads/{id}', [AdController::class, 'show']);
-    Route::put('/user/ads/{id}', [AdController::class, 'update']); 
-    Route::delete('/user/ads/{id}', [AdController::class, 'destroy']);
 });
 
-Route::apiResource('posts', PostController::class);
-Route::apiResource('ads', AdController::class);
 ?>
