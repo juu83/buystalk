a44c3a8c22c1628732833319f98ac997c7b8760c
root <root@laptop.localdomain>
2026-04-20 10:20:02 +0200

    fix : app.vue

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,7 @@
 <script setup>
 const { token, fetchUser } = useAuth()
 
+
 onMounted(async () => {
   if (token.value) {
     await fetchUser()
@@ -10,20 +11,9 @@ onMounted(async () => {
 })
 </script>
 
+
 <template>
-  <div class="min-h-screen bg-gray-100 flex justify-center">
-    
-    <div class="w-full max-w-md bg-white min-h-screen shadow-2xl border-x border-gray-200">
-      
-      <header class="sticky top-0 bg-white/80 backdrop-blur-md border-b p-4 z-50 flex justify-between items-center">
-        <h1 class="font-black text-blue-600 text-xl tracking-tighter">BuyStalk</h1>
-        <NuxtLink v-if="token" to="/account" class="text-xs font-bold uppercase text-gray-400">Compte</NuxtLink>
-      </header>
-
-      <main class="p-4">
-        <NuxtPage />
-      </main>
-
-    </div>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
