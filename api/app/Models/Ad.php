5628258ecb564e91b4a26d5d718a0bb2e09fba5a
root <root@laptop.localdomain>
2026-04-20 09:20:51 +0200

    fix : ad model

--- api/app/Models/Ad.php
+++ api/app/Models/Ad.php
@@ -2,18 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 class Ad extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'city', 'lat', 'lng', 'user_id', 'image'];
+    protected $fillable = [
+        'title', 'description', 'image', 'city', 'lat', 'lng', 'user_id'
+    ];
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-
 }
