18d5c5e28aea19ba5ac59e3a0b9f39d79bbb9f51
root <root@laptop.localdomain>
2026-04-20 09:56:26 +0200

    fix : databaseSeeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -3,10 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Post;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Post;
-use App\Models\Ad;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,16 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // create a known user for local debugging
+        User::factory()->create([
+            'email' => 'test@test.com',
+            'password' => Hash::make('password'),
+            'firstname' => 'Test',
+            'lastname' => 'User'
+        ]);
+
         $users = User::factory(10)->create();
 
         Post::factory(50)->make()->each(function ($post) use ($users) {
             $post->user_id = $users->random()->id;
             $post->save();
-        });
-
-        Ad::factory(30)->make()->each(function ($ad) use ($users) {
-            $ad->user_id = $users->random()->id;
-            $ad->save();
         });
     }
 }
